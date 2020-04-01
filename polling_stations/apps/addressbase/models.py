fee1bd0894200dc4fb9c3049fb6388faf9fe6258
GeoWill <roper.william@gmail.com>
2020-04-01 10:28:15 +0100

    Add polling_station_id field to UprnToCouncil model

--- polling_stations/apps/addressbase/models.py
+++ polling_stations/apps/addressbase/models.py
@@ -31,6 +31,7 @@ class UprnToCouncil(models.Model):
 
     uprn = models.CharField(primary_key=True, max_length=12)
     lad = models.CharField(blank=True, max_length=9)
+    polling_station_id = models.CharField(blank=True, max_length=255)
 
 
 class Blacklist(models.Model):
