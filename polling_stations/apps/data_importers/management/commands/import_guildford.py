327f73e2b3246405091ede4a130c6ba7f6a65727
GeoWill <roper.william@gmail.com>
2021-04-13 15:47:53 +0100

    Fix for Guildford

--- polling_stations/apps/data_importers/management/commands/import_guildford.py
+++ polling_stations/apps/data_importers/management/commands/import_guildford.py
@@ -12,3 +12,20 @@ class Command(BaseHalaroseCsvImporter):
         if record.housepostcode in ["GU1 1AD", "GU23 7JL"]:
             return None
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Update from Council
+        if (
+            record.pollingstationnumber == "39"
+            and record.pollingstationname == "The Green Room"
+        ):
+            record = record._replace(
+                pollingstationname="East Horsley Village Hall",
+                pollingstationaddress_1="Kingston Avenue",
+                pollingstationaddress_2="East Horsley",
+                pollingstationaddress_3="Leatherhead",
+                pollingstationaddress_4="",
+                pollingstationpostcode="KT24 6QT",
+            )
+
+        return super().station_record_to_dict(record)
